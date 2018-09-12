dceb12c520c86596c9c303d72b6f32fd31ba19be
Nabilahmed Patel <nabilpatel11@gmail.com>
2018-09-12 10:01:52 -0500

    Upating google api call with CURL

--- Controllers/ImpactsController.php
+++ Controllers/ImpactsController.php
@@ -17,12 +17,33 @@ class ImpactsController extends Controller
 			$this->view->render('Views/error.html');
 		}
 
- 		$address = $_POST['address']; // Google HQ
-        $prepAddr = str_replace(' ','+',$address);
-        $geocode = file_get_contents('https://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false');
-        $output= json_decode($geocode);
-        $latitude = $output->results[0]->geometry->location->lat;
-        $longitude = $output->results[0]->geometry->location->lng;
+
+		/** Fixing Latitude and Longitude in case google api doesn't work */
+		$latitude = 30.19631195;
+		$longitude = -97.73080444;
+
+		/* USING file_get_contents */
+		// $address = $_POST['address']; // Google HQ
+		// $prepAddr = str_replace(' ','+',$address);
+		// $response = file_get_contents('https://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false');
+
+		/* Using CURL */
+		$url = "https://maps.google.com/maps/api/geocode/json?address=".$_POST['address']."&sensor=false";
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		$response = curl_exec($ch);
+		curl_close($ch);
+
+        $output= json_decode($response);
+        if ($output) {
+	        $latitude = $output->results[0]->geometry->location->lat;
+	        $longitude = $output->results[0]->geometry->location->lng;
+	    }
 
         $repository = new ImpactRepository;
         $dataset = $repository->getUsersByDuration($longitude, $latitude);
